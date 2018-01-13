ad2b94faf7f9a90acbff6001161cb7e428b4981c
dcw3 <dcw3@princeton.edu>
2018-01-13 15:54:19 -0500

    Motors Shell

---
+++ motors.h
@@ -0,0 +1,21 @@
+/*
+This module is used to interface with the motors.
+*/
+
+#include "globals.h"
+
+// double is power: should be between -1 and 1
+// (1 being max power, -1 being max power in the opposite direction)
+void setMotor(int direction, double power) {
+	// TODO
+}
+
+// any initialization that's needed
+void init() {
+
+}
+
+// reset anything that needs to be reset?
+reset() {
+
+}
