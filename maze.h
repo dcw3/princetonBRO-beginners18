9b21df7acea6c2a2123baae73060594a046b6538
dcw3 <dcw3@princeton.edu>
2018-01-14 19:56:14 -0500

    Maze First Draft

---
+++ maze.h
@@ -0,0 +1,25 @@
+/*
+This module manages the maze navigation.
+*/
+
+struct MAZE {
+	// Data values
+}
+
+typedef struct MAZE *MAZE_T;
+
+MAZE_T init() {
+
+}
+
+void updateMaze() {
+
+}
+
+void updatePosition() {
+
+}
+
+void getDirection() {
+	
+}
