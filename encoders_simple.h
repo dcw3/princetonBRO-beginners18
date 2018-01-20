86bff30cf35d243d3ceee0dd2fab8a91d0cb367c
Kyle Johnson <kyleaj@princeton.edu>
2018-01-20 16:35:31 -0500

    Finished draft of this file. Still need to correct the circumference values and test this. Also, I'm not completely sure if I referenced global constants correctly.

--- encoders_simple.h
+++ encoders_simple.h
@@ -5,58 +5,31 @@ This module interfaces with the encoders.
 #include "globals.h"
 #include "pololu/encoders.h"
 
-// struct holds the # of encoders clicks seen so far
-struct Encoders {
-  int n_clicks_north = 0;
-  int n_clicks_east = 0;
-  int n_clicks_south = 0;
-  int n_clicks_west = 0;
-  double rev_per_click = 1.0/48.0;
-  double cirumference = 48; // in meters TODO: Get right value
-};
-
-const int forwardAPin = 1;
-const int forwardBPin = 2;
-const int leftAPin = 1;
-const int leftBPin = 2;
-
-// this is just a pointer to the Encoders struct
-// used for passing Encoders to/from functions
-typedef struct Encoders *Encoders_T;
+const double rev_per_click = 1.0/12.0; // Seems small but listed here: https://www.pololu.com/product/3081/resources
+const double circumference_left = 48; // in meters TODO: Get right value
+const double circumference_up = 48; // in meters TODO: Get right value
+const char forwardAPin = PD1;
+const char forwardBPin = PD2;
+const char leftAPin = PD3;
+const char leftBPin = PD4;
 
 void init() {
-  // TODO
-  //encoders_init(forwardAPin, forwardBPin, leftAPin, leftBPin);
-  encoders_init('a', 'b', 'c', 'd');
+  encoders_init(forwardAPin, forwardBPin, leftAPin, leftBPin);
 }
 
 // anything needed during reset
 void reset() {
-  // TODO
-  // Example: Encoders_T->n_clicks = 0?
+  encoders_get_counts_and_reset_m1();
+    encoders_get_counts_and_reset_m2();
 }
 
 // return distance traveled since init occurred... This might be a mess right now.
 double getCumulDist(int direction) {
-  // TODO
-  int clicks = 0;
   switch(direction) {
-
    case NORTH  :
-      clicks = n_clicks_north;
-      break;
-  
-   case SOUTH  :
-      clicks = n_clicks_south;
-      break;
+       return (rev_per_click * encoders_get_counts_m1() * circumference_up);
 
     case EAST  :
-      clicks = n_clicks_east;
-      break;
-
-    case WEST  :
-      clicks = n_clicks_west;
-      break;
+      return (rev_per_click * encoders_get_counts_m2() * circumference_left);
     }
-    return (Encoders.rev_per_click * clicks * Encoders.cirumference);
 }
