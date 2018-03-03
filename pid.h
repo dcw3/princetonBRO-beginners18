bb59de014175b7e5ca9e5fe37c44b52d016884b0
rshi159 <rshi0611@gmail.com>
2018-03-03 16:42:17 -0500

    going to test this

--- pid.h
+++ pid.h
@@ -5,21 +5,28 @@ This module creates PID loop objects.
 #include "globals.h"
 
 struct PID {
-	//
 	double kP ,kI, kD;
 	double integral;
 	double setPoint;
-	double prevMeasure;
+	//double prevMeasure;
+	double derivative;
+	double prevError;
+	bool isFirstUpdate;
 	// TODO: add flag to detect first update?
 };
 
 typedef struct PID *PID_T;
 // return a PID loop object
 PID_T init(double kP, double kI, double kD) {
-	PID *p;
+	struct PID *p;
 	p->kP = kP;
 	p->kI = kI;
 	p->kD = kD;
+	p->integral = 0;// TODO
+	p->setPoint = 0;
+	p->derivative = 0;
+	p->prevError = 0;
+	p->isFirstUpdate = true;
 	return p;
 };
 
@@ -27,18 +34,24 @@ PID_T init(double kP, double kI, double kD) {
 void reset(PID_T pid) {
 	pid->integral = 0;// TODO
 	pid->setPoint = 0;
-	pid->prevMeasure = 0;
+	pid->derivative = 0;
+	pid->prevError = 0;
 }
 
 // update PID_T 
-double update(PID_T pid, double val, double dt) {
+//double update(PID_T pid, double val, double dt) {
+void update(PID_T pid, double val, double dt) {
 	double error = pid->setPoint - val;
-
+	pid->integral = pid->integral + error * dt;
+	pid->derivative = (error - pid->prevError) / dt;
+	pid->prevError = error;
+	pid->isFirstUpdate = false;
 	//deriv = currentMeasure - prevMeasure;
 }
 
 // return PID value
-getVal(PID_T pid) {
+double getVal(PID_T pid) {
+	return (pid->kP * pid->prevError) + (pid->kI * pid->integral) + (pid->derivative * pid->kD);
 	// TODO: return PID value
 }
 
