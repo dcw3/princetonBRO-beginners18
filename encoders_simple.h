5ae83fe8863ba036364b309d59af9cd8a520eb12
Kyle Johnson <kyleaj@princeton.edu>
2018-03-08 13:54:36 -0500

    Compiles in Arduino IDE but has not yet been tested with a motor.

--- encoders_simple.h
+++ encoders_simple.h
@@ -1,46 +1,45 @@
 /*
 This module interfaces with the encoders.
+ This is written in C++ as it works with the Arduino an is also much easier to work with.
 */
 
 #include "globals.h"
-#include "pololu/encoders.h"
+#include "Encoder.h"
 
-const double rev_per_click = 1.0/12.0; // Seems small but listed here: https://www.pololu.com/product/3081/resources
-const double circumference_left = 48; // in meters TODO: Get right value
-const double circumference_up = 48; // in meters TODO: Get right value
-const char forwardAPin = PD1;
-const char forwardBPin = PD2;
-const char leftAPin = PD3;
-const char leftBPin = PD4;
-int numCalls;
-double northDirection;
-double eastDirection;
+const double PI = 3.1415926536;
+const double rev_per_click = 1.0/12.0; // Confirmed correct
+const double circumference_left = 48*PI/1000; // in meters
+const double circumference_up = 38*PI/1000; // in meters
+const int forwardAPin = 2;
+const int forwardBPin = 4;
+const int leftAPin = 3;
+const int leftBPin = 5;
 
-void init() {
-  encoders_init(forwardAPin, forwardBPin, leftAPin, leftBPin);
-    numCalls = 0;
-    northDirection = 0;
-    eastDirection = 0;
-}
+Encoder upDisp(forwardAPin, forwardBPin);
+Encoder leftDisp(leftAPin, leftBPin);
+
+// No initiate function necessary. Use reset to set encoders back to zero.
 
 // anything needed during reset
 void reset() {
-  encoders_get_counts_and_reset_m1();
-    encoders_get_counts_and_reset_m2();
+    upDisp = Encoder(forwardAPin, forwardBPin);
+    leftDisp = Encoder(leftAPin, leftBPin);
 }
 
-// return distance traveled since init occurred... This might be a mess right now.
+// return distance traveled since init occurred. This can work as a coordinate system. Returns distance in meters.
 double getCumulDist(int direction) {
-    if (numCalls % 50 == 0) {
-        northDirection = (rev_per_click * encoders_get_counts_and_reset_m1() * circumference_up);
-        eastDirection = (rev_per_click * encoders_get_counts_and_reset_m2() * circumference_left);
-    }
-    numCalls++;
   switch(direction) {
    case NORTH  :
-       return (northDirection + (rev_per_click * encoders_get_counts_m1() * circumference_up));
+       return (rev_per_click * upDisp.read() * circumference_up);
 
     case EAST  :
-      return (eastDirection + (rev_per_click * encoders_get_counts_m2() * circumference_left));
+      return (rev_per_click * leftDisp.read() * circumference_left);
+
+      case SOUTH  :
+          return -(rev_per_click * upDisp.read() * circumference_up);
+
+      case WEST  :
+          return -(rev_per_click * leftDisp.read() * circumference_left);
     }
+    return 1.0/0.0;
 }
