405c40d59e002d691c9bfbc90faf7b0031420910
aiqtidar <aiqtidar@princeton.edu>
2018-03-10 17:26:46 -0500

    Wrote Main

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,6 @@
+#ifndef MAIN2_H_INCLUDED
+#define MAIN2_H_INCLUDED
+
 #import "maze.h"
 #import "movement.h"
 #import "globals.h"
@@ -6,19 +9,23 @@ void setup() {
   // put your setup code here, to run once:
   Maze_T maze = init();
   Movement_T movement = movement_init();
-  int coord1 = 0, coord2 = 0, d = -1;
+  int coord1 = 0, c oord2 = 0, d = -1;
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
+
   if (((coord1 == 7) || (coord1 == 8)) && ((coord2 == 7) || (coord2 == 8))) {
-    //???
+  //implement back-trace
+    while ((coord1 != 0)&&(coord2 != 0){
+        d = getDirection(maze, coord1, coord2);
+        moved(d);
+        updatePosition(maze, GetCoord1(), GetCoord1());
+    }
   }
+
   d = getDirection(maze, coord1, coord2);
-  move_square(d);
-  if (d == NORTH) {coord2++;}
-  if (d == EAST) {coord1++;}
-  if (d == SOUTH) {coord2--;}
-  if (d == WEST) {coord1--;}
-  updatePosition(maze, coord1, coord2);
-}
+  moved(d);
+  updatePosition(maze, GetCoord1(), GetCoord1());
+
+
+#endif // MAIN2_H_INCLUDED
