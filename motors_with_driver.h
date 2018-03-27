6ad8dbdf0b03e1f4587ced75ec7a7a01d9131eb5
Kyle Johnson <kyleaj@princeton.edu>
2018-03-27 01:42:26 -0400

    Fixed scaling, some typos, and reordered functions so that it does verify/compile in Arduino. BTW, Motor Driver wiring is set up right now and I wrote about it in the Google Drive file that contains the Motor Encoder diagram. But here's an alright guide: http://www.instructables.com/id/How-to-use-the-L298-Motor-Driver-Module-Arduino-Tu/.

--- motors_with_driver.h
+++ motors_with_driver.h
@@ -11,29 +11,19 @@ NOTE: Hasn't been tested yet.
 const float batteryVoltage = 7.4;
 const float goodVoltage = 5.0; // Good speed for robot voltage
 int maxPower = 0;
-const int LUDICROUS_SPEED = 255; // Max power, to fast for normal movement.
+const int LUDICROUS_SPEED = 255; // Max power, too fast for normal movement.
 
 const int motorAPinA = 6; // Fix these when wiring robot. Must be pins with ~ by them (support PWM).
 const int motorAPinB = 7;
 const int motorBPinA = 8;
 const int motorBPinB = 9;
 
-const int scaleA = 1; // Motors not same speed? Change these to scale them.
+const int scaleA = 1; // Motors not same speed? Change these to scale them. Must be less than one.
 const int scaleB = 1;
-
-// double is power: should be between -1 and 1
-// (1 being max power, -1 being max power in the opposite direction)
-void setMotor(int direction, double power) {
-    switch (direction) {
-        case NORTH : northMove(power); break;
-        case SOUTH : northMove(power * -1); break;
-        case EAST :  eastMove(power); break;
-        case WEST : westMove(power * -1); break;
-    }
-}
 
 // Make it easier
 void northMove(double power) {
+    power = power * scaleA;
     if (power < 0) {
         analogWrite(motorAPinA, 0);
         analogWrite(motorAPinB, (int)(power * maxPower));
@@ -44,12 +34,24 @@ void northMove(double power) {
 }
 
 void eastMove(double power) {
+    power = power * scaleB;
     if (power < 0) {
         analogWrite(motorBPinA, 0);
         analogWrite(motorBPinB, (int)(power * maxPower));
     } else {
         analogWrite(motorBPinA, (int)(power * maxPower));
         analogWrite(motorBPinB, 0);
+    }
+}
+
+// double is power: should be between -1 and 1
+// (1 being max power, -1 being max power in the opposite direction)
+void setMotor(int direction, double power) {
+    switch (direction) {
+        case NORTH : northMove(power); return;
+        case SOUTH : northMove(power * -1); return;
+        case EAST :  eastMove(power); return;
+        case WEST :  eastMove(power * -1); return;
     }
 }
 
@@ -68,7 +70,7 @@ void init() {
 }
 
 // reset anything that needs to be reset - Turns off motors.
-reset() {
+void reset() {
     analogWrite(motorAPinA, 0);
     analogWrite(motorAPinB, 0);
     analogWrite(motorBPinA, 0);
