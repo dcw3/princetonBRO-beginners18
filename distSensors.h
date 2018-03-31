dea401973a639ed564baed144bbfa8abffc8d1e3
khatna <bold@princeton.edu>
2018-03-31 16:05:03 -0400

    deleted unused code

--- distSensors.h
+++ distSensors.h
@@ -19,10 +19,10 @@ const double INIT_W_OFFSET = 0;
 // threshold distance for detectWall (mm)
 double const WALL = 70;
 
-// initializes senso2rr
+// initializes sensor
 void initSensor(VL6180X* sensor) {
 	(*sensor).init();
-  (*sensor).configureDefault();
+	(*sensor).configureDefault();
 	(*sensor).setTimeout(500);
 }
 
@@ -39,47 +39,34 @@ VL6180X *ptrSensorWest = &sensorWest;
 VL6180X *ptrSensorEast = &sensorEast;
 
 void setupSensors() {
-  // shutdown pins
-  pinMode(NORTH_SHUTDOWN, OUTPUT);
-  pinMode(WEST_SHUTDOWN, OUTPUT);
-  pinMode(EAST_SHUTDOWN, OUTPUT);
+	// shutdown pins
+	pinMode(NORTH_SHUTDOWN, OUTPUT);
+	pinMode(WEST_SHUTDOWN, OUTPUT);
+	pinMode(EAST_SHUTDOWN, OUTPUT);
 
-  // hold each sensor in shutdown initially
-  digitalWrite(NORTH_SHUTDOWN, LOW);
-  digitalWrite(WEST_SHUTDOWN, LOW);
-  digitalWrite(EAST_SHUTDOWN, LOW);
-  delay(10);
+	 // hold each sensor in shutdown initially
+	digitalWrite(NORTH_SHUTDOWN, LOW);
+	digitalWrite(WEST_SHUTDOWN, LOW);
+	digitalWrite(EAST_SHUTDOWN, LOW);
+	delay(10);
 
-  // wake sensors and change address one by one
-  (*ptrSensorSouth).setAddress(0x25); // SOUTH is the first to change - doesnt need to be in reset
-  delay(10);
-  digitalWrite(WEST_SHUTDOWN, HIGH);
-  (*ptrSensorWest).setAddress(0x25);
-  delay(10);
-  digitalWrite(EAST_SHUTDOWN, HIGH);
-  (*ptrSensorEast).setAddress(0x27);
-  delay(10);
-  digitalWrite(NORTH_SHUTDOWN, HIGH); // NORTH maintains default address
+	// wake sensors and change address one by one
+	(*ptrSensorSouth).setAddress(0x25); // SOUTH is the first to change - doesnt need to be in reset
+	delay(10);
+	digitalWrite(WEST_SHUTDOWN, HIGH);
+	(*ptrSensorWest).setAddress(0x25);
+	delay(10);
+	digitalWrite(EAST_SHUTDOWN, HIGH);
+	(*ptrSensorEast).setAddress(0x27);
+	delay(10);
+	digitalWrite(NORTH_SHUTDOWN, HIGH); // NORTH maintains default address
 
-  // init all sensors
-  initSensor(ptrSensorNorth);
-  initSensor(ptrSensorSouth);
-  initSensor(ptrSensorWest);
-  initSensor(ptrSensorEast);
+	// init all sensors
+	initSensor(ptrSensorNorth);
+	initSensor(ptrSensorSouth);
+	initSensor(ptrSensorWest);
+	initSensor(ptrSensorEast);
 }
-
-// this struct holds the offsets needed for each offset
-// feel free to add other stuff to this struct as well
-// unused
-struct DistSensors {
-	double northOffset = INIT_N_OFFSET;
-	double eastOffset = INIT_E_OFFSET;
-	double southOffset = INIT_S_OFFSET;
-	double westOffset = INIT_W_OFFSET;
-};
-// this is just a pointer to the DistSensors struct
-// used for passing DistSensors to/from functions
-typedef struct DistSensors *DistSensors_T;
 
 // Return the distance reading from the given direction
 double getDist(int direction) {
@@ -93,23 +80,6 @@ double getDist(int direction) {
 	case EAST:
 		return sensorEast.readRangeSingleMillimeters();
 	}
-}
-
-// Calibrate the distance sensor by updating that offset
-// probably not used yet: will be used in the init() function later
-void calibrate(DistSensors_T sensors, double currentDist, int direction) {
-	// TODO (unused)
-}
-
-// initialization: return a DistSensors object
-DistSensors_T initDistSensor() {
-	// TODO (unused)
-}
-
-// reset the DistSensors object
-// this may be empty but it's included just in case we do need it
-void reset(DistSensors_T) {
-	// TODO
 }
 
 // return 1 if there is a wall, 0 if there is not a wall
