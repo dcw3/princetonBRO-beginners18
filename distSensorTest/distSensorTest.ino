f8b706a8e7a1bc18628ff94afcdfd1f09f225e62
rshi159 <rshi0611@gmail.com>
2018-04-06 01:09:37 -0400

    using example distance sensor code

--- distSensorTest/distSensorTest.ino
+++ distSensorTest/distSensorTest.ino
@@ -1,17 +1,41 @@
-#include "distSensors.h"
-void setup() {
-  setupSensors();
-  Serial.begin(9600);
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <VL6180X.h>
 
+VL6180X sensor;
+
+void setup() 
+{
+  Serial.begin(9600);
+  Wire.begin();
+  
+  sensor.init();
+  sensor.configureDefault();
+  sensor.setTimeout(500);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
-  for(int j = 0; j < 4; j++) {
-      Serial.println(getDist(j));
-      Serial.print("\t");
-  }
+void loop() 
+{ 
+  Serial.print(sensor.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  
   Serial.println();
 }
+//#include "VL6180X.cpp"
+//void setup() {
+//  Serial.begin(9600);
+//  VL6180X();
+//  void configureDefault();
+//  // put your setup code here, to run once:
+//
+//}
+//
+//void loop() {
+//  // put your main code here, to run repeatedly:
+//  delay(1000);
+//  for(int j = 0; j < 4; j++) {
+//    int dist = readReg(uint16_t reg);
+//      Serial.println(dist);
+//      Serial.print("\t");
+//  }
+//  Serial.println();
+//}
